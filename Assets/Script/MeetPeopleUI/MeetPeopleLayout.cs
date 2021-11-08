e7313cc3d69ddb555e8f602d9bad8c72463e1c8b
Migea <qiaozhi22@Gmail.com>
2021-11-07 22:58:28 -0500

    meetPeopleLayoutFunctional

--- Assets/Script/MeetPeopleUI/MeetPeopleLayout.cs
+++ Assets/Script/MeetPeopleUI/MeetPeopleLayout.cs
@@ -1,10 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Linq;
 
-[ExecuteAlways]
-public class MeetPeopleLayout : MonoBehaviour
+public class MeetPeopleLayout : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public GameObject tagList;
+    public GameObject IdleImage;
+
+    private void Awake()
+    {
+        tagList.gameObject.SetActive(false);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(37.5f, 237.5f);
+        IdleImage.GetComponent<RectTransform>().sizeDelta = new Vector2(37.5f, 237.5f);
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<RectTransform>().sizeDelta = new Vector2(37.5f, 237.5f);
+        IdleImage.GetComponent<RectTransform>().sizeDelta = new Vector2(37.5f, 237.5f);
+        tagList.gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<RectTransform>().sizeDelta = new Vector2(37.5f * 2, 237.5f);
+        IdleImage.GetComponent<RectTransform>().sizeDelta = new Vector2(37.5f * 2, 237.5f);
+
+        tagList.gameObject.SetActive(true);
+    }
 
 }
