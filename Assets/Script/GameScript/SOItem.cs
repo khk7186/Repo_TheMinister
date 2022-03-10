58a2e6d4767ca7540e447605d33a0bdb7a0d3d1c
nathanbao <zyxbsh@gmail.com>
2022-03-10 15:59:47 -0500

    Craft Update

--- Assets/Script/GameScript/SOItem.cs
+++ Assets/Script/GameScript/SOItem.cs
@@ -129,6 +129,7 @@ public class SOItem : ScriptableObject
                 ItemName.剑,
                 ItemName.戟,
                 ItemName.有缺口的武器,
+                ItemName.铁片戟
             }
         },
         {
@@ -283,7 +284,6 @@ public class SOItem : ScriptableObject
         {
             ItemType.材料, new List<ItemName>()
             {
-                ItemName.铁片戟,
                 ItemName.丝绸,
                 ItemName.五香粉,
                 ItemName.良姜,
@@ -408,11 +408,12 @@ public class SOItem : ScriptableObject
                 ItemName.剑,
                 ItemName.戟,
                 ItemName.弓,
+                ItemName.白银枪,
+                ItemName.黄金弓,
+                ItemName.大砍刀,
                 ItemName.穿石烈风弓,
                 ItemName.烈火斩云刀,
-                ItemName.紫木雷电枪,
-                ItemName.皇之剑,
-                ItemName.天霸方天戟,
+                ItemName.紫木雷电枪
             }
         },
         {
@@ -423,27 +424,22 @@ public class SOItem : ScriptableObject
                 ItemName.剑,
                 ItemName.戟,
                 ItemName.弓,
+                ItemName.白银枪,
+                ItemName.黄金弓,
+                ItemName.大砍刀,
                 ItemName.穿石烈风弓,
                 ItemName.烈火斩云刀,
                 ItemName.紫木雷电枪,
-                ItemName.皇之剑,
-                ItemName.天霸方天戟,
-                ItemName.百胜刀,
-                ItemName.落日神弓,
-                ItemName.擎天枪,
-                ItemName.龙源剑,
+                ItemName.落日神弓
             }
         },
         {
             BuildingType.机关阁, new List<ItemName>()
             {
-                ItemName.背剑,
-                ItemName.小刀,
-                ItemName.袖箭,
-                ItemName.峨眉刺,
+
                 ItemName.血滴子,
                 ItemName.袖炮,
-                ItemName.金钱镖,
+                ItemName.金钱镖
 
             }
             
@@ -451,14 +447,9 @@ public class SOItem : ScriptableObject
         {
             BuildingType.武侯楼, new List<ItemName>()
             {
-                ItemName.背剑,
-                ItemName.小刀,
-                ItemName.袖箭,
-                ItemName.峨眉刺,
                 ItemName.血滴子,
                 ItemName.袖炮,
                 ItemName.金钱镖,
-                ItemName.浪击连弩,
                 ItemName.暴雨梨花针
 
             }
@@ -469,23 +460,14 @@ public class SOItem : ScriptableObject
             {
                 ItemName.奉子丹,
                 ItemName.金疮药,
-                ItemName.长生不老药,
-                ItemName.十全大补丸,
-                ItemName.沸腾散,
-                ItemName.龙虎丹,
-                ItemName.洗髓丹,
-                ItemName.止血膏,
-                ItemName.阴阳玄龙丹,
-                ItemName.天机造化丹
+                ItemName.止血膏
             }
         },
         {
             BuildingType.纺织铺, new List<ItemName>()
             {
-                ItemName.蓑衣,
+
                 ItemName.羽衣,
-                ItemName.布衣,
-                ItemName.丝绸,
                 ItemName.长袖装,
 
             }
@@ -493,38 +475,32 @@ public class SOItem : ScriptableObject
         {
             BuildingType.梭织坊, new List<ItemName>()
             {
-                ItemName.蓑衣,
+
                 ItemName.羽衣,
-                ItemName.布衣,
-                ItemName.丝绸,
                 ItemName.长袖装,
                 ItemName.云纹袍,
-                ItemName.朱户衣,
+                ItemName.朱户衣
 
             }
         },
         {
             BuildingType.长安织造, new List<ItemName>()
             {
-                ItemName.蓑衣,
+
                 ItemName.羽衣,
-                ItemName.布衣,
-                ItemName.丝绸,
                 ItemName.长袖装,
                 ItemName.黄金甲,
                 ItemName.云纹袍,
-                ItemName.朱户衣,
+                ItemName.朱户衣
             }
         },
         {
             BuildingType.服装店, new List<ItemName>()
             {
-                ItemName.蓑衣,
+
                 ItemName.皮甲,
                 ItemName.铁甲,
                 ItemName.羽衣,
-                ItemName.布衣,
-                ItemName.丝绸,
                 ItemName.长袖装,
                 ItemName.锦绣华服
             }
@@ -532,12 +508,9 @@ public class SOItem : ScriptableObject
         {
             BuildingType.玉服华裳, new List<ItemName>()
             {
-                ItemName.蓑衣,
                 ItemName.皮甲,
                 ItemName.铁甲,
                 ItemName.羽衣,
-                ItemName.布衣,
-                ItemName.丝绸,
                 ItemName.长袖装,
                 ItemName.锦绣华服
             }
@@ -625,7 +598,7 @@ public class SOItem : ScriptableObject
                 ItemName.沸腾散,
                 ItemName.龙虎丹,
                 ItemName.洗髓丹,
-                ItemName.止血膏,
+                ItemName.止血膏
 
             }
         },
@@ -739,6 +712,10 @@ public class SOItem : ScriptableObject
             {
                 ItemName.飞蝗石,
                 ItemName.树叶,
+                ItemName.背剑,
+                ItemName.小刀,
+                ItemName.袖箭,
+                ItemName.峨眉刺,
 
             }
 
@@ -746,10 +723,12 @@ public class SOItem : ScriptableObject
         {
             BuildingType.武侯楼, new List<ItemName>()
             {
+                ItemName.飞蝗石,
+                ItemName.树叶,
                 ItemName.背剑,
                 ItemName.小刀,
                 ItemName.袖箭,
-                ItemName.峨眉刺,
+                ItemName.峨眉刺
 
 
             }
@@ -783,7 +762,9 @@ public class SOItem : ScriptableObject
         {
             BuildingType.纺织铺, new List<ItemName>()
             {
-
+                ItemName.蓑衣,
+                ItemName.布衣,
+                ItemName.丝绸
 
             }
         },
@@ -791,7 +772,6 @@ public class SOItem : ScriptableObject
             BuildingType.梭织坊, new List<ItemName>()
             {
                 ItemName.蓑衣,
-                ItemName.羽衣,
                 ItemName.布衣,
                 ItemName.丝绸
 
@@ -802,7 +782,6 @@ public class SOItem : ScriptableObject
             BuildingType.长安织造, new List<ItemName>()
             {
                 ItemName.蓑衣,
-                ItemName.羽衣,
                 ItemName.布衣,
                 ItemName.丝绸
 
@@ -1333,15 +1312,15 @@ public class SOItem : ScriptableObject
         {ItemName.剑,new List<ItemName>(){ItemName.铜矿,ItemName.铁矿}},
         {ItemName.戟,new List<ItemName>(){ItemName.铁矿,ItemName.铁矿}},
         {ItemName.弓,new List<ItemName>(){ItemName.木头,ItemName.绳子}},
-        {ItemName.穿石烈风弓,new List<ItemName>(){ItemName.弓,ItemName.祖母绿}},
-        {ItemName.烈火斩云刀,new List<ItemName>(){ItemName.刀,ItemName.鸽血红}},
-        {ItemName.紫木雷电枪,new List<ItemName>(){ItemName.枪,ItemName.紫水晶}},
+        {ItemName.穿石烈风弓,new List<ItemName>(){ItemName.黄金弓, ItemName.祖母绿}},
+        {ItemName.烈火斩云刀,new List<ItemName>(){ItemName.大砍刀, ItemName.鸽血红}},
+        {ItemName.紫木雷电枪,new List<ItemName>(){ItemName.白银枪, ItemName.紫水晶}},
         {ItemName.皮甲,new List<ItemName>(){ItemName.布衣,ItemName.皮革}},
         {ItemName.铁甲,new List<ItemName>(){ItemName.布衣,ItemName.铁矿}},
         {ItemName.黄金甲,new List<ItemName>(){ItemName.铁甲,ItemName.有破损的黄金}},
         {ItemName.云纹袍,new List<ItemName>(){ItemName.长袖装,ItemName.绣花针}},
         {ItemName.朱户衣,new List<ItemName>(){ItemName.云纹袍,ItemName.红宝石}},
-        {ItemName.锦绣华服,new List<ItemName>(){ItemName.羽衣,ItemName.吵闹的鹦鹉}},
+        {ItemName.锦绣华服,new List<ItemName>(){ItemName.羽衣,ItemName.祖母绿,ItemName.红宝石}},
         {ItemName.袖炮,new List<ItemName>(){ItemName.袖箭,ItemName.火炮}},
         {ItemName.金钱镖,new List<ItemName>(){ItemName.袖箭,ItemName.有破损的黄金}},
         {ItemName.金疮药,new List<ItemName>(){ItemName.黄精,ItemName.八角莲}},
@@ -1351,7 +1330,16 @@ public class SOItem : ScriptableObject
         {ItemName.洗髓丹,new List<ItemName>(){ItemName.轻粉,ItemName.核桃}},
         {ItemName.龙虎丹,new List<ItemName>(){ItemName.当归,ItemName.核桃}},
         {ItemName.阴阳玄龙丹,new List<ItemName>(){ItemName.白花蛇舌草,ItemName.过山龙}},
-        {ItemName.天机造化丹,new List<ItemName>(){ItemName.白花蛇舌草,ItemName.星辰花}}
+        {ItemName.落日神弓,new List<ItemName>(){ItemName.穿石烈风弓, ItemName.紫木雷电枪}},
+        {ItemName.白银枪,new List<ItemName>(){ItemName.银矿, ItemName.枪}},
+        {ItemName.黄金弓,new List<ItemName>(){ItemName.有破损的黄金, ItemName.弓}},
+        {ItemName.大砍刀,new List<ItemName>(){ItemName.铁矿, ItemName.木头, ItemName.刀}},
+        {ItemName.血滴子,new List<ItemName>(){ItemName.绳子, ItemName.铆钉,ItemName.峨眉刺}},
+        {ItemName.暴雨梨花针,new List<ItemName>(){ItemName.峨眉刺, ItemName.浪击连弩}},
+        {ItemName.奉子丹,new List<ItemName>(){ItemName.当归, ItemName.人参}},
+        {ItemName.止血膏,new List<ItemName>(){ItemName.三七, ItemName.血风藤}},
+        {ItemName.羽衣,new List<ItemName>(){ItemName.吵闹的鹦鹉, ItemName.丝绸}},
+        {ItemName.长袖装,new List<ItemName>(){ItemName.布衣, ItemName.丝绸}}
     };
     public Sprite NullSprite;
 
