934c8d3308d8e26388f7c4024150b58e6ef5bdb5
Migea <qiaozhi22@Gmail.com>
2023-04-18 15:51:18 -0400

    fix 2

--- Assets/Script/MoneyCollection/MoneyCollectAnimationHandler.cs
+++ Assets/Script/MoneyCollection/MoneyCollectAnimationHandler.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEditor.iOS.Xcode;
 using UnityEngine.UI;
 
 public class MoneyCollectAnimationHandler
