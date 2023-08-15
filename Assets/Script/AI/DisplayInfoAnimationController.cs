e77e7da66fb0b93c912ff18093a3315915187f15
Migea <qiaozhi22@Gmail.com>
2023-08-14 20:26:29 -0400

    show tag in game

---
+++ Assets/Script/AI/DisplayInfoAnimationController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+public class DisplayInfoAnimationController : MonoBehaviour
+{
+    public Animator animator;
+    public Text NameText;
+    public List<Image> TagImages;
+
+    public bool FakeData = false;
+    public List<string> FakeTags;
+    public string FakeName;
+
+    public void Start()
+    {
+        if (FakeData)
+        {
+            List<Tag> tags = new List<Tag>();
+            foreach (string tag in FakeTags)
+            {
+                tags.Add((Tag)Enum.Parse(typeof(Tag), tag));
+            }
+            SetupTags(tags);
+            SetupName(FakeName);
+            return;
+        }
+        var ai = GetComponent<DefaultInGameAI>();
+        if (ai != null)
+        {
+            SetupTags(ai.character.tagList);
+            SetupName(ai.character.CharacterName);
+        }
+    }
+    public void SetupName(string Name)
+    {
+        NameText.text = Name;
+    }
+    public void SetupTags(List<Tag> tags)
+    {
+        int index = 0;
+        foreach (Tag tag in tags)
+        {
+            TagImages[index].sprite = TagSpecUI.FindTagSprite(tag);
+            index++;
+        }
+    }
+    public void Show()
+    {
+        animator.Play("Show");
+        NameText.gameObject.SetActive(true);
+    }
+    public void Hide()
+    {
+        animator.Play("Hide");
+        NameText.gameObject.SetActive(false);
+    }
+    public void OnMouseEnter()
+    {
+        Show();
+    }
+    public void OnMouseExit()
+    {
+        Hide();
+    }
+}
