ba32edc44dbbe8aa29868d33deebb9d0c15e5ba7
Migea <qiaozhi22@Gmail.com>
2023-09-05 13:57:30 -0400

    Update Combat.cs

--- Assets/Script/Combat/new/Combat.cs
+++ Assets/Script/Combat/new/Combat.cs
@@ -61,6 +61,7 @@ public class Combat : MonoBehaviour
             {
                 Destroy(interactable.line.gameObject);
                 ccu.target = null;
+                ccu.currentAction = Action.NoSelect;
             }
         }
     }
