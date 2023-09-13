dc31cd4366cecde17865abcb881d5ed7d392295e
Migea <qiaozhi22@Gmail.com>
2023-09-13 15:06:40 -0400

    quest giver pointer

---
+++ Assets/Script/GameSaveAndLoad/QuestGiverPointer.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGiverPointer : MonoBehaviour
+{
+    public QuestGiverAI questGiverAI = null;
+}
