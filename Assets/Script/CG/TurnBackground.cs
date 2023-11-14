c6877a9fa9704d0a2802313389cc91c0e0f46045
Migea <qiaozhi22@Gmail.com>
2023-11-14 17:45:26 -0500

    Update TurnBackground.cs

--- Assets/Script/CG/TurnBackground.cs
+++ Assets/Script/CG/TurnBackground.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class TurnBackground : MonoBehaviour
 {
-    
+    public bool TurnItOn = false;
+    public bool TriggerOnEnable = false;
+
+    private void OnEnable()
+    {
+        if (TriggerOnEnable)
+        {
+            Turn();
+        }
+    }
+    public void Turn()
+    {
+        if (!TurnItOn)
+        {
+            FindObjectOfType<BackgoundMusicController>().GetComponent<AudioSource>().Stop();
+        }
+        else
+        {
+            FindObjectOfType<BackgoundMusicController>().GetComponent<AudioSource>().Play();
+        }
+    }
 }
