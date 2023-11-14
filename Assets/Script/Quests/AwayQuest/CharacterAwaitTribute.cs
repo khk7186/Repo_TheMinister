191b4e18ead1cec452e45749bd51f1f7152cad9d
Migea <qiaozhi22@Gmail.com>
2023-11-14 18:36:31 -0500

    Update CharacterAwaitTribute.cs

--- Assets/Script/Quests/AwayQuest/CharacterAwaitTribute.cs
+++ Assets/Script/Quests/AwayQuest/CharacterAwaitTribute.cs
@@ -9,19 +9,21 @@ public class CharacterAwaitTribute : MonoBehaviour
     public int WaitTime;
     public int AlreadyWait = 0;
     public UnityEvent @event;
+    private bool destroyNext = false;
 
     public bool EndWait => AlreadyWait > WaitTime;
 
     public void TimePlus()
     {
+        if (destroyNext) CharacterAwaitTributeManager.Instance.FinishTribute(this);
         AlreadyWait += 1;
         if (EndWait)
         {
             if (@event != null)
             {
                 @event.Invoke();
+                destroyNext = true;
             }
-            CharacterAwaitTributeManager.Instance.FinishTribute(this);
         }
     }
 }
