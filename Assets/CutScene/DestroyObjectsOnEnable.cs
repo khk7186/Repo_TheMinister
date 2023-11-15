bd898180a2af03025d100c0e8f5b2454555fe252
Migea <qiaozhi22@Gmail.com>
2023-11-15 16:32:52 -0500

    Update DestroyObjectsOnEnable.cs

--- Assets/CutScene/DestroyObjectsOnEnable.cs
+++ Assets/CutScene/DestroyObjectsOnEnable.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DestroyObjectsOnEnable : MonoBehaviour
 {
     public List<GameObject> GameObjectsToDestroy;
     public bool DestroyOnEnable = false;
+    public string destroyMainProfile = string.Empty;
     private void OnEnable()
     {
         if (DestroyOnEnable)
@@ -18,6 +20,11 @@ public class DestroyObjectsOnEnable : MonoBehaviour
         foreach (GameObject go in GameObjectsToDestroy)
         {
             Destroy(go);
+        }
+        if (destroyMainProfile != string.Empty)
+        {
+            var unit = FindObjectsOfType<MainEventUnitProfile>().FirstOrDefault(x => x.profileName == destroyMainProfile);
+            if (unit != null) Destroy(unit.gameObject);
         }
     }
 }
