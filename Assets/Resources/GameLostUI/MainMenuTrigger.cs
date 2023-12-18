4ea95079508e4ed4514e996c53e0d9b51f826bcb
Migea <qiaozhi22@Gmail.com>
2023-12-18 11:04:28 -0500

    main menu trigger

---
+++ Assets/Resources/GameLostUI/MainMenuTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuTrigger : MonoBehaviour
+{
+    public void JumpToMainMenu()
+    {
+        string path = $"SceneTransPrefab/{SceneType.MainGame}/{SceneType.MainGame}Animation";
+        var canvas = Instantiate(Resources.Load<Canvas>("SceneTransPrefab/Canvas"));
+        DontDestroyOnLoad(canvas);
+        var animation = Instantiate(Resources.Load<SceneTransController>(path), canvas.transform);
+        animation.transDelegate = NextStep;
+        animation.Close();
+    }
+    IEnumerator NextStep()
+    {
+        var animation = FindObjectOfType<SceneTransController>();
+        yield return new WaitUntil(() => animation.transition.GetCurrentAnimatorStateInfo(0).IsName("Wait"));
+        SceneManager.LoadScene(0);
+        yield return WaitUntilSceneLoad.WaitUntilScene(1);
+        yield return new WaitForSeconds(0.5f);
+        animation.Open();
+    }
+}
