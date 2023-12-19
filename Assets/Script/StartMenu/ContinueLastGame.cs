2ea774cfada07f9667787c93565207408d4856a8
Migea <qiaozhi22@Gmail.com>
2023-12-19 12:26:25 -0500

    settings

--- Assets/Script/StartMenu/ContinueLastGame.cs
+++ Assets/Script/StartMenu/ContinueLastGame.cs
@@ -12,6 +12,7 @@ public class ContinueLastGame : MonoBehaviour
     {
         var db = FindObjectOfType<GameSaveUIController>(true).GameSaveDatabase;
         GetComponentInChildren<Text>().color = new Color(225, 215, 170, 0);
+        db.FindAllSaves();
         yield return new WaitUntil(() => db.gameSaves.Count > 0);
         gameSave = db.FindLatest();
         GetComponentInChildren<Text>().color = new Color(225, 215, 170, 1);
