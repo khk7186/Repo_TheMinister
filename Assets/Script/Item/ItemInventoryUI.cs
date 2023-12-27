0dfdc1412dcc071dc9f2035d0bf63b7b7d1b8925
Migea <qiaozhi22@Gmail.com>
2023-12-27 11:37:15 -0500

    iteminv now set filter on start

--- Assets/Script/Item/ItemInventoryUI.cs
+++ Assets/Script/Item/ItemInventoryUI.cs
@@ -13,6 +13,8 @@ public class ItemInventoryUI : MonoBehaviour, IPointerClickHandler
     public GameObject[] OffForEmptyWarn;
     public Rarerity currentRareFilter = Rarerity.Null;
     public string currentTypeFilter = string.Empty;
+    public Transform rareButtons;
+    public Transform typeButtons;
 
     private void Awake()
     {
@@ -28,10 +30,12 @@ public class ItemInventoryUI : MonoBehaviour, IPointerClickHandler
             if (itemInventory.ItemDict.Count <= 0)
             {
                 EmptyInvView();
+                CloseAllFilters();
                 return;
             }
         }
         SetUp();
+        FilterSetup();
     }
     public void EmptyInvView()
     {
@@ -57,6 +61,7 @@ public class ItemInventoryUI : MonoBehaviour, IPointerClickHandler
             //Debug.Log(key);
             target.GetComponent<ItemUI>().Setup(key, itemInventory.ItemDict[key]);
         }
+
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -165,6 +170,73 @@ public class ItemInventoryUI : MonoBehaviour, IPointerClickHandler
             else
             {
                 child.gameObject.SetActive(false);
+            }
+        }
+    }
+    public void CloseAllFilters()
+    {
+        foreach (Transform child in rareButtons)
+        {
+            child.gameObject.SetActive(false);
+        }
+        foreach (Transform child in typeButtons)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+    public void FilterSetup()
+    {
+        Debug.Log("filter");
+        List<string> rareList = new List<string>();
+        List<ItemType> typeList = new List<ItemType>();
+        foreach (Transform child in transform)
+        {
+            var childRare = child.GetComponent<ItemUI>().framRarity.ToString();
+            if (!rareList.Contains(childRare)) rareList.Add(childRare);
+            foreach (var item in SOItem.ItemTypeDict)
+            {
+                if (item.Value.Contains(child.GetComponent<ItemUI>().ItemName))
+                {
+                    if (!typeList.Contains(item.Key)) typeList.Add(item.Key);
+                    break;
+                }
+            }
+        }
+        Debug.Log(string.Join(",", typeList));
+        Debug.Log(string.Join(",", rareList));
+        foreach (Transform child in rareButtons)
+        {
+            if (child.name == "All")
+            {
+                gameObject.SetActive(true);
+            }
+            else if (rareList.Contains(child.gameObject.name))
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+        foreach (Transform child in typeButtons)
+        {
+            Debug.Log(child.gameObject.name);
+            if (child.name == "All")
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                ItemType itemType = (ItemType)Enum.Parse(typeof(ItemType), child.gameObject.name);
+                if (typeList.Contains(itemType))
+                {
+                    child.gameObject.SetActive(true);
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
             }
         }
     }
