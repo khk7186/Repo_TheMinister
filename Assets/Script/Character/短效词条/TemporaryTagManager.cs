fdb8564fede8da3e27c41b19391625024fc742b0
Migea <qiaozhi22@Gmail.com>
2023-12-28 19:28:35 -0500

    Update TemporaryTagManager.cs

--- Assets/Script/Character/短效词条/TemporaryTagManager.cs
+++ Assets/Script/Character/短效词条/TemporaryTagManager.cs
@@ -41,6 +41,7 @@ public class TemporaryTagManager : MonoBehaviour, IDiceRollEvent
                         removeList.Add(tempTag);
                     }
                 }
+                character.GetComponent<Character>().UpdateVariables();
                 foreach (var tempTag in removeList)
                 {
                     list.Remove(tempTag);
