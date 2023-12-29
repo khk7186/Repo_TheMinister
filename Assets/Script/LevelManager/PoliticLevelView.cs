cdcb19aea4e1533225cad0aa6097da7159af14de
Migea <qiaozhi22@Gmail.com>
2023-12-28 20:37:11 -0500

    politics

---
+++ Assets/Script/LevelManager/PoliticLevelView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoliticLevelView : MonoBehaviour
+{
+    public Image fillImage;
+    public Text LevelText;
+
+    
+}
--- Assets/Script/LevelManager/PoliticSlot.cs
+++ Assets/Script/LevelManager/PoliticSlot.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PoliticSlot : MonoBehaviour
+public class PoliticSlot : MonoBehaviour,ICharacterSelect
 {
+    public Image CharacterHead = null;
     public Character characterOnHold = null;
-    
+    public int exp;
+    public List<Tag> requestTags;
+    public List<PoliticSlot> preSlots = new List<PoliticSlot>();
+
+    public void ChooseCharacter(Character character)
+    {
+        characterOnHold = character;
+    }
+
+    public void CloseInventory()
+    {
+        
+    }
+
+    public void CloseInventory(CharacterUI current)
+    {
+        
+    }
+
+    public void PutCharacterOn(Character character)
+    {
+        characterOnHold = character;
+    }
+
+    public void SetupSlotIcon()
+    {
+        
+    }
 }
