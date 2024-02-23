b465d4f4bee1b591aa8c74de391451cc32a89914
Migea <qiaozhi22@Gmail.com>
2024-02-22 21:00:08 -0500

    animation

--- Assets/Script/LevelManager/Pages/PoliticPage.cs
+++ Assets/Script/LevelManager/Pages/PoliticPage.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PoliticPage : MonoBehaviour
 {
+    RectTransform rectTransform => GetComponent<RectTransform>();
+    float originX = 31f;
+    float zeroX = 210f;
+    float duration = 0.2f;
+
     public void Show()
     {
-        gameObject.SetActive(true);
+        rectTransform.DOAnchorPosX(originX, duration).OnComplete(()=> gameObject.SetActive(true));
+        
     }
     public void Hide()
     {
-        gameObject.SetActive(false);
+        rectTransform.DOAnchorPosX(zeroX, duration).OnComplete(() => gameObject.SetActive(false));
     }
 }
