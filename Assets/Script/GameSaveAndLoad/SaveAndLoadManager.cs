98f5eac0d87b8346c5fe24999788b4b2fffd9956
Migea <qiaozhi22@Gmail.com>
2024-02-23 14:08:48 -0500

    Update SaveAndLoadManager.cs

--- Assets/Script/GameSaveAndLoad/SaveAndLoadManager.cs
+++ Assets/Script/GameSaveAndLoad/SaveAndLoadManager.cs
@@ -77,6 +77,7 @@ namespace SaveSystem
             gameEventManager.Reset();
             playerCharacterInventory.Clear();
             characterAwaitTributeManager.Reset();
+            GeneralTrackingViewManager.Instance.Reset();
             politicActionUI.Reset();
             questionAIManager.Reset();
             map.ReloadPlayer();
@@ -108,7 +109,7 @@ namespace SaveSystem
             playerQuestJournal = player.GetComponent<QuestJournal>();
             GameSaveDatabase.currentGameSave = save;
             LoadEvent.Load(this, GameSaveDatabase.currentGameSave);
-            
+
         }
         public static void SetGameLoadMode(GameSave save)
         {
