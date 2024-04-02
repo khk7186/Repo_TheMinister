e8a81b0f49b3a49e81dfb764a0e701bb027e87f5
Migea <qiaozhi22@Gmail.com>
2024-04-01 21:20:59 -0400

    Update SpawnAfterAwayGuest.cs

--- Assets/Script/Quests/AwayQuest/SpawnAfterAwayGuest.cs
+++ Assets/Script/Quests/AwayQuest/SpawnAfterAwayGuest.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 public class SpawnAfterAwayGuest : MonoBehaviour
 {
     public string guestName = string.Empty;
-    public void Start()
-    {
-        FindObjectOfType<SaveAndLoadManager>().gameGuests.Add(this);
-    }
-    public void OnDestroy()
-    {
-        FindObjectOfType<SaveAndLoadManager>().gameGuests.Remove(this);
-    }
-    public void OnDisable()
-    {
-        FindObjectOfType<SaveAndLoadManager>().gameGuests.Remove(this);
-    }
+    //public void Start()
+    //{
+    //    FindObjectOfType<SaveAndLoadManager>().gameGuests.Add(this);
+    //}
+    //public void OnDestroy()
+    //{
+    //    FindObjectOfType<SaveAndLoadManager>().gameGuests.Remove(this);
+    //}
+    //public void OnDisable()
+    //{
+    //    FindObjectOfType<SaveAndLoadManager>().gameGuests.Remove(this);
+    //}
 }
