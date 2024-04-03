e5bd3ea2bf37034b53fa317657c06135500c4654
Migea <qiaozhi22@Gmail.com>
2024-04-03 15:16:53 -0400

    steam wish list

--- Assets/Script/BetaContent/LinkToSteamWishlist.cs
+++ Assets/Script/BetaContent/LinkToSteamWishlist.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 using Steamworks;
 using UnityEngine.EventSystems;
 
-public class LinkToSteamWishlist : MonoBehaviour,IPointerClickHandler
+public class LinkToSteamWishlist : MonoBehaviour, IPointerClickHandler
 {
     public string url = "https://store.steampowered.com/app/1234567";
     /// <summary>
     /// 加入愿望单, url就是游戏的商店页地址
     /// </summary>
+    public bool Large = false;
+    public Animator animator;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Large == false)
+        {
+            animator.Play("Big");
+            Large = true;
+        }
+        else
+        {
+            animator.Play("Small");
+            Large = false;
+        }
+    }
+    private void OnEnable()
+    {
+        animator.Play("Big");
+        Large = true;
+    }
     public void AddToSteamWish(string url)
     {
 #if DISABLESTEAMWORKS || UNITY_EDITOR
@@ -20,8 +40,8 @@ public class LinkToSteamWishlist : MonoBehaviour,IPointerClickHandler
 #endif
     }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        AddToSteamWish(url);
-    }
+    //public void OnPointerClick(PointerEventData eventData)
+    //{
+    //    AddToSteamWish(url);
+    //}
 }
