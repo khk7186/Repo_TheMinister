bf9703ad2687a3885a5e9888c378a92db6fd6da6
khk7186 <30765149+khk7186@users.noreply.github.com>
2024-06-07 11:37:11 +0900

    Delete Assets/CutScene directory

--- Assets/CutScene/IStopPlayer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IStopPlayer
-{
-    int CurrentBlock { get; }
-    GameObject gameObject { get; }
-}
